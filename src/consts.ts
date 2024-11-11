1180c1a51e7917752dcd78f31a7aedefca4e6381
克喵 <kemiaofx@gmail.com>
2024-11-11 18:40:24 +0800

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -2,16 +2,16 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 // Site title and description
-export const SITE_LANG = "en";
-export const SITE_TAB = "Frosti";
-export const SITE_TITLE = "Frosti 🧊";
-export const SITE_DESCRIPTION = "A blog template";
+export const SITE_LANG = "zh";
+export const SITE_TAB = "克喵爱吃卤面";
+export const SITE_TITLE = "克喵的个人博客";
+export const SITE_DESCRIPTION = "一个致力于分享资源的博客~";
 export const DATE_FORMAT = "ddd MMM DD YYYY";
 
 // User profile information
-export const USER_NAME = "EveSunMaple";
+export const USER_NAME = "克喵爱吃卤面";
 export const USER_SITE = "https://frosti.saroprock.com"
-export const USER_AVATAR = "/profile.webp";
+export const USER_AVATAR = "https://bu.dusays.com/2024/11/05/6729d52b375b9.jpg";
 
 // Server and transition settings
 export const SERVER_URL = "https://demo.saroprock.com";
@@ -28,32 +28,32 @@ export const CODE_THEME = {
 
 // Menu items for navigation
 export const menuItems = [
-  { id: "home", text: "Home", href: "/", svg: "home", target: "_self" }, // Home page
-  { id: "about", text: "About", href: "/about", svg: "about", target: "_self" }, // About page
+  { id: "home", text: "主页", href: "/", svg: "home", target: "_self" }, // Home page
+  { id: "about", text: "关于", href: "/about", svg: "about", target: "_self" }, // About page
   {
     id: "blog",
-    text: "Blogs",
+    text: "博客",
     href: "/blog",
     svg: "blog",
     target: "_self",
     subItems: [
       {
         id: "all",
-        text: "All blogs",
+        text: "所有的博客",
         href: "/blog",
         svg: "post",
         target: "_self",
       }, // All blog
       {
         id: "tech",
-        text: "Tech blogs",
+        text: "资源分享的博客",
         href: "/blog/categories/tech",
         svg: "cube",
         target: "_self",
       }, // Technology category
       {
         id: "life",
-        text: "Life blogs",
+        text: "日常生活的博客",
         href: "/blog/categories/life",
         svg: "heart",
         target: "_self",
@@ -62,22 +62,22 @@ export const menuItems = [
   }, // Blog page with sub-items
   {
     id: "project",
-    text: "Project",
+    text: "项目",
     href: "/project",
     svg: "project",
     target: "_self",
   }, // Projects page
   {
     id: "friend",
-    text: "Friend",
+    text: "朋友",
     href: "/friend",
     svg: "friend",
     target: "_self",
   }, // Friends page
   {
     id: "contact",
-    text: "Contact",
-    href: "mailto:contact.evesunmaple@outlook.com", // Contact email
+    text: "交流",
+    href: "mailto:contact.3149261770@qq.com", // Contact email
     target: "_blank", // Open in a new tab
     svg: "contact",
   },
@@ -92,13 +92,13 @@ export const socialIcons = [
     svg: "support",
   },
   {
-    href: "https://github.com/EveSunMaple",
+    href: "https://ifdian.net/order/create?plan_id=585f8be8978211efbdc852540025c377&product_type=0&remark=",
     ariaLabel: "Github",
     title: "Github",
     svg: "github",
   },
   {
-    href: "https://space.bilibili.com/438392347",
+    href: "https://space.bilibili.com/396923563",
     ariaLabel: "BiliBili",
     title: "BiliBili",
     svg: "bilibili",
